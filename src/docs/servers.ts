aec11ded003acb9b5668fede30c3110091aac453
Alex Galhardo <aleexgvieira@gmail.com>
2022-01-13 18:24:35 -0300

    chore: deleted docs folder

--- src/docs/servers.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    servers:[
-        {
-            url:"http://localhost:3000",
-            description:"GalhardoAPP LocalHost Server"
-        },
-    ]
-}
--- src/docs/tags.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    tags:{
-        name:'PUBLIC API',
-        name:'ADMIN API',
-        name:'PROFILE API',
-    }
-}
