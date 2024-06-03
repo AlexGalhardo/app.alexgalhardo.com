e84cc4e2c8f4d728a977e50fbe098167c53340ce
Alex Galhardo <aleexgvieira@gmail.com>
2024-06-03 10:00:28 -0300

    fix: format

--- src/config/smtp.ts
+++ src/config/smtp.ts
@@ -1,13 +1,13 @@
 import nodemailer from "nodemailer";
 
 const Resend = nodemailer.createTransport({
-	host: "smtp.resend.com",
-	secure: true,
-	port: 465,
-	auth: {
-		user: "resend",
-		pass: Bun.env.RESEND_API_KEY,
-	},
+    host: "smtp.resend.com",
+    secure: true,
+    port: 465,
+    auth: {
+        user: "resend",
+        pass: Bun.env.RESEND_API_KEY,
+    },
 });
 
 export default Resend;
--- src/utils/NodeMailer.ts
+++ src/utils/NodeMailer.ts
@@ -8,174 +8,174 @@ import { ShopTransactionDTO, SubscriptionTransactionDTO, SendContactDTO } from "
 import DateTime from "./DateTime";
 
 export default class NodeMailer {
-	static async sendContact({ name, email, subject, message }: SendContactDTO) {
-		const filePath = path.join(__dirname, "../views/emails/contact.html");
+    static async sendContact({ name, email, subject, message }: SendContactDTO) {
+        const filePath = path.join(__dirname, "../views/emails/contact.html");
 
-		const source = fs.readFileSync(filePath, "utf-8").toString();
+        const source = fs.readFileSync(filePath, "utf-8").toString();
 
-		const template = handlebars.compile(source);
+        const template = handlebars.compile(source);
 
-		const replacements = {
-			name,
-			email,
-			subject,
-			message,
-		};
+        const replacements = {
+            name,
+            email,
+            subject,
+            message,
+        };
 
-		const htmlBody = template(replacements);
+        const htmlBody = template(replacements);
 
-		const messageSend = await Resend.sendMail({
-			from: email,
-			to: process.env.APP_EMAIL,
-			subject: `Galhardo APP Contact: ${subject} from ${name}`,
-			text: subject,
-			html: htmlBody,
-		});
-	}
+        const messageSend = await Resend.sendMail({
+            from: email,
+            to: process.env.APP_EMAIL,
+            subject: `Galhardo APP Contact: ${subject} from ${name}`,
+            text: subject,
+            html: htmlBody,
+        });
+    }
 
-	static async sendShopTransaction({
-		transaction_id,
-		card_id,
-		paid,
-		products,
-		products_amount,
-		shipping_fee,
-		total_amount,
-		shipping_address_zipcode,
-		shipping_address_street,
-		shipping_address_neighborhood,
-		shipping_address_city,
-		shipping_address_state,
-	}: ShopTransactionDTO) {
-		const filePath = path.join(__dirname, "../views/emails/shop_transaction.html");
+    static async sendShopTransaction({
+        transaction_id,
+        card_id,
+        paid,
+        products,
+        products_amount,
+        shipping_fee,
+        total_amount,
+        shipping_address_zipcode,
+        shipping_address_street,
+        shipping_address_neighborhood,
+        shipping_address_city,
+        shipping_address_state,
+    }: ShopTransactionDTO) {
+        const filePath = path.join(__dirname, "../views/emails/shop_transaction.html");
 
-		const source = fs.readFileSync(filePath, "utf-8").toString();
+        const source = fs.readFileSync(filePath, "utf-8").toString();
 
-		const template = handlebars.compile(source);
+        const template = handlebars.compile(source);
 
-		const replacements = {
-			transaction_id,
-			payment_method: card_id,
-			paid: paid,
-			products: JSON.stringify(products),
-			total_products: products_amount,
-			shipping_fee,
-			amount: total_amount,
-			shipping_address_zipcode,
-			shipping_address_street,
-			shipping_address_neighborhood,
-			shipping_address_city,
-			shipping_address_state,
-			created_at: DateTime.getNow(),
-		};
+        const replacements = {
+            transaction_id,
+            payment_method: card_id,
+            paid: paid,
+            products: JSON.stringify(products),
+            total_products: products_amount,
+            shipping_fee,
+            amount: total_amount,
+            shipping_address_zipcode,
+            shipping_address_street,
+            shipping_address_neighborhood,
+            shipping_address_city,
+            shipping_address_state,
+            created_at: DateTime.getNow(),
+        };
 
-		const htmlBody = template(replacements);
+        const htmlBody = template(replacements);
 
-		await Resend.sendMail({
-			from: process.env.APP_EMAIL,
-			to: "aleexgvieira@gmail.com", // shopTransactionObject.customer.email,
-			subject: `Galhardo APP: Shop Transaction Success!`,
-			html: htmlBody,
-		});
+        await Resend.sendMail({
+            from: process.env.APP_EMAIL,
+            to: "aleexgvieira@gmail.com", // shopTransactionObject.customer.email,
+            subject: `Galhardo APP: Shop Transaction Success!`,
+            html: htmlBody,
+        });
 
-		Resend.close();
-	}
+        Resend.close();
+    }
 
-	static async sendSubscriptionTransaction({
-		transaction_id,
-		status,
-		plan_name,
-		plan_amount,
-		card_id,
-		card_brand,
-		card_exp_month,
-		card_exp_year,
-		card_last4,
-		current_period_start,
-		current_period_end,
-	}: SubscriptionTransactionDTO) {
-		const filePath = path.join(__dirname, "../views/emails/subscription_transaction.html");
+    static async sendSubscriptionTransaction({
+        transaction_id,
+        status,
+        plan_name,
+        plan_amount,
+        card_id,
+        card_brand,
+        card_exp_month,
+        card_exp_year,
+        card_last4,
+        current_period_start,
+        current_period_end,
+    }: SubscriptionTransactionDTO) {
+        const filePath = path.join(__dirname, "../views/emails/subscription_transaction.html");
 
-		const source = fs.readFileSync(filePath, "utf-8").toString();
+        const source = fs.readFileSync(filePath, "utf-8").toString();
 
-		const template = handlebars.compile(source);
+        const template = handlebars.compile(source);
 
-		const replacements = {
-			transaction_id,
-			status,
-			plan_name,
-			amount: Number(plan_amount / 100).toFixed(2),
-			card_id,
-			card_brand,
-			card_exp_month,
-			card_exp_year,
-			card_last4,
-			current_period_start,
-			current_period_end,
-			created_at: DateTime.getNow(),
-		};
+        const replacements = {
+            transaction_id,
+            status,
+            plan_name,
+            amount: Number(plan_amount / 100).toFixed(2),
+            card_id,
+            card_brand,
+            card_exp_month,
+            card_exp_year,
+            card_last4,
+            current_period_start,
+            current_period_end,
+            created_at: DateTime.getNow(),
+        };
 
-		const htmlBody = template(replacements);
+        const htmlBody = template(replacements);
 
-		await Resend.sendMail({
-			from: process.env.APP_EMAIL,
-			to: "aleexgvieira@gmail.com", // subsTransactionObject.customer.email,
-			subject: `Galhardo APP: Subscription Transaction Success!`,
-			html: htmlBody,
-		});
+        await Resend.sendMail({
+            from: process.env.APP_EMAIL,
+            to: "aleexgvieira@gmail.com", // subsTransactionObject.customer.email,
+            subject: `Galhardo APP: Subscription Transaction Success!`,
+            html: htmlBody,
+        });
 
-		Resend.close();
-	}
+        Resend.close();
+    }
 
-	static async sendConfirmEmailLink(email: string, confirmEmailToken: string) {
-		const confirmEmailLinkURL = `${process.env.APP_URL}/confirmEmail/${email}/${confirmEmailToken}`;
+    static async sendConfirmEmailLink(email: string, confirmEmailToken: string) {
+        const confirmEmailLinkURL = `${process.env.APP_URL}/confirmEmail/${email}/${confirmEmailToken}`;
 
-		const filePath = path.join(__dirname, "../views/emails/confirm_email.html");
+        const filePath = path.join(__dirname, "../views/emails/confirm_email.html");
 
-		const source = fs.readFileSync(filePath, "utf-8").toString();
+        const source = fs.readFileSync(filePath, "utf-8").toString();
 
-		const template = handlebars.compile(source);
+        const template = handlebars.compile(source);
 
-		const replacements = {
-			confirmEmailLinkURL,
-		};
+        const replacements = {
+            confirmEmailLinkURL,
+        };
 
-		const htmlBody = template(replacements);
+        const htmlBody = template(replacements);
 
-		await Users.createConfirmEmailToken(email, confirmEmailToken);
+        await Users.createConfirmEmailToken(email, confirmEmailToken);
 
-		await Resend.sendMail({
-			from: process.env.APP_EMAIL,
-			to: email,
-			subject: `GALHARDO APP: Confirm Your Email!`,
-			html: htmlBody,
-		});
+        await Resend.sendMail({
+            from: process.env.APP_EMAIL,
+            to: email,
+            subject: `GALHARDO APP: Confirm Your Email!`,
+            html: htmlBody,
+        });
 
-		Resend.close();
-	}
+        Resend.close();
+    }
 
-	static async sendForgetPasswordLink(email: string, resetPasswordToken: string) {
-		const resetPasswordLinkURL = `${process.env.APP_URL}/resetPassword/${email}/${resetPasswordToken}`;
+    static async sendForgetPasswordLink(email: string, resetPasswordToken: string) {
+        const resetPasswordLinkURL = `${process.env.APP_URL}/resetPassword/${email}/${resetPasswordToken}`;
 
-		const filePath = path.join(__dirname, "../views/emails/forget_password.html");
+        const filePath = path.join(__dirname, "../views/emails/forget_password.html");
 
-		const source = fs.readFileSync(filePath, "utf-8").toString();
+        const source = fs.readFileSync(filePath, "utf-8").toString();
 
-		const template = handlebars.compile(source);
+        const template = handlebars.compile(source);
 
-		const replacements = {
-			resetPasswordLinkURL,
-		};
+        const replacements = {
+            resetPasswordLinkURL,
+        };
 
-		const htmlBody = template(replacements);
+        const htmlBody = template(replacements);
 
-		await Resend.sendMail({
-			from: process.env.APP_EMAIL,
-			to: email,
-			subject: `GALHARDO APP: Recover Your Password!`,
-			html: htmlBody,
-		});
+        await Resend.sendMail({
+            from: process.env.APP_EMAIL,
+            to: email,
+            subject: `GALHARDO APP: Recover Your Password!`,
+            html: htmlBody,
+        });
 
-		Resend.close();
-	}
+        Resend.close();
+    }
 }
