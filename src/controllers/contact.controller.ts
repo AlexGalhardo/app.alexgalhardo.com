9013e0ffb9f44a81a2712fdf616f4e6ad928c8cc
Alex Galhardo <aleexgvieira@gmail.com>
2024-06-03 09:51:18 -0300

    fix: format

--- src/controllers/contact.controller.ts
+++ src/controllers/contact.controller.ts
@@ -6,47 +6,47 @@ import NodeMailer from "../utils/NodeMailer";
 // import TelegramBOTLogger from "../utils/TelegramBOTLogger";
 
 export default class ContactController {
-	static getViewContact(req: Request, res: Response) {
-		res.render("pages/contact", {
-			flash_success: req.flash("success"),
-			flash_warning: req.flash("warning"),
-			user: global.SESSION_USER,
-			header: Header.contact(),
-			captcha: res.recaptcha,
-			csrfToken: req.csrfToken(),
-		});
-	}
+    static getViewContact(req: Request, res: Response) {
+        res.render("pages/contact", {
+            flash_success: req.flash("success"),
+            flash_warning: req.flash("warning"),
+            user: global.SESSION_USER,
+            header: Header.contact(),
+            captcha: res.recaptcha,
+            csrfToken: req.csrfToken(),
+        });
+    }
 
-	static async postContact(req: Request, res: Response, next: NextFunction) {
-		try {
-			const errors = validationResult(req);
+    static async postContact(req: Request, res: Response, next: NextFunction) {
+        try {
+            const errors = validationResult(req);
 
-			if (!req.recaptcha?.error) {
-				if (!errors.isEmpty()) {
-					req.flash("warning", `${errors.array()[0].msg}`);
-					return res.redirect("/contact");
-				}
-			} else {
-				req.flash("warning", `Invalid Recaptcha!`);
-				return res.redirect("/contact");
-			}
+            if (!req.recaptcha?.error) {
+                if (!errors.isEmpty()) {
+                    req.flash("warning", `${errors.array()[0].msg}`);
+                    return res.redirect("/contact");
+                }
+            } else {
+                req.flash("warning", `Invalid Recaptcha!`);
+                return res.redirect("/contact");
+            }
 
-			const { name, email, subject, message } = req.body;
+            const { name, email, subject, message } = req.body;
 
-			const contactObject = {
-				name,
-				email,
-				subject,
-				message,
-			};
+            const contactObject = {
+                name,
+                email,
+                subject,
+                message,
+            };
 
-			await NodeMailer.sendContact(contactObject);
-			// await TelegramBOTLogger.logContact(contactObject);
+            await NodeMailer.sendContact(contactObject);
+            // await TelegramBOTLogger.logContact(contactObject);
 
-			req.flash("success", "Message Send!");
-			return res.redirect("/contact");
-		} catch (error) {
-			next(error);
-		}
-	}
+            req.flash("success", "Message Send!");
+            return res.redirect("/contact");
+        } catch (error) {
+            next(error);
+        }
+    }
 }
