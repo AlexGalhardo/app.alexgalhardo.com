bff4b30ee02617a065255a32cd8093137c65a177
Alex Galhardo <aleexgvieira@gmail.com>
2024-06-07 08:59:21 -0300

    fix: error on games.controller

--- src/controllers/web/games.controller.ts
+++ src/controllers/web/games.controller.ts
@@ -10,74 +10,71 @@ import UsersRepository from "../../repositories/users.repository";
 import edge from "../../config/edge";
 
 export default class GamesController {
-    static async getViewGames(req: Request, res: Response) {
-        try {
-            const game = await GamesRepository.getRandom();
-            const totalGames = await GamesRepository.getTotal();
-            const totalBooks = await BooksRepository.getTotal();
-            const totalMovies = await MoviesRepository.getTotal();
-            const totalTVShows = await TVShowsRepository.getTotal();
-            const totalItensShopCart = await UsersRepository.getTotalItensShopCart();
-            game.price = NumberHelper.toFloat(game.price as unknown as string);
+	static async getViewGames(req: Request, res: Response) {
+		try {
+			const game = await GamesRepository.getRandom();
+			const totalGames = await GamesRepository.getTotal();
+			const totalBooks = await BooksRepository.getTotal();
+			const totalMovies = await MoviesRepository.getTotal();
+			const totalTVShows = await TVShowsRepository.getTotal();
+			game.price = NumberHelper.toFloat(game.price as unknown as string);
 
-            return res.setHeader("Content-Type", "text/html").end(
-                await edge.render("pages/games", {
-                    flash_success: req.flash("success").length ? req.flash("success")[0] : null,
-                    flash_warning: req.flash("warning").length ? req.flash("warning")[0] : null,
-                    game,
-                    totalGames,
-                    totalBooks,
-                    totalMovies,
-                    totalTVShows,
-                    totalItensShopCart: totalItensShopCart ?? 0,
-                    user: global.SESSION_USER,
-                    app_url: process.env.APP_URL,
-                    header: Header.games(),
-                }),
-            );
-        } catch (error: any) {
-            res.status(500).send(error.message);
-        }
-    }
+			return res.setHeader("Content-Type", "text/html").end(
+				await edge.render("pages/games", {
+					flash_success: req.flash("success").length ? req.flash("success")[0] : null,
+					flash_warning: req.flash("warning").length ? req.flash("warning")[0] : null,
+					game,
+					totalGames,
+					totalBooks,
+					totalMovies,
+					totalTVShows,
+					user: global.SESSION_USER,
+					app_url: process.env.APP_URL,
+					header: Header.games(),
+				}),
+			);
+		} catch (error: any) {
+			res.status(500).send(error.message);
+		}
+	}
 
-    static async getSearchGameTitle(req: Request, res: Response) {
-        try {
-            const searchGameTitle = req.query.title as string;
+	static async getSearchGameTitle(req: Request, res: Response) {
+		try {
+			const searchGameTitle = req.query.title as string;
 
-            if (!searchGameTitle) {
-                return res.redirect("/");
-            }
+			if (!searchGameTitle) {
+				return res.redirect("/");
+			}
 
-            let searchedGames = await GamesRepository.searchTitle(searchGameTitle);
+			let searchedGames = await GamesRepository.searchTitle(searchGameTitle);
 
-            if (!searchedGames.length) {
-                req.flash("warning", `No games found from search: ${searchGameTitle}! Recommending a Random Game`);
-                return res.redirect("/");
-            }
+			if (!searchedGames.length) {
+				req.flash("warning", `No games found from search: ${searchGameTitle}! Recommending a Random Game`);
+				return res.redirect("/");
+			}
 
-            if (searchedGames.length > 1) {
-                return res.setHeader("Content-Type", "text/html").end(
-                    await edge.render("pages/games", {
-                        flash_success: `${
-                            searchedGames.length
-                        } Games Found From Search Title: ${String(searchGameTitle).toUpperCase()}`,
-                        games: searchedGames,
-                        user: global.SESSION_USER,
-                        header: Header.games(),
-                    }),
-                );
-            }
+			if (searchedGames.length > 1) {
+				return res.setHeader("Content-Type", "text/html").end(
+					await edge.render("pages/games", {
+						flash_success: `${searchedGames.length
+							} Games Found From Search Title: ${String(searchGameTitle).toUpperCase()}`,
+						games: searchedGames,
+						user: global.SESSION_USER,
+						header: Header.games(),
+					}),
+				);
+			}
 
-            return res.setHeader("Content-Type", "text/html").end(
-                await edge.render("pages/games", {
-                    flash_success: `1 Game Found From Search Title: ${String(searchGameTitle).toUpperCase()}`,
-                    games: searchedGames,
-                    user: global.SESSION_USER,
-                    header: Header.games(),
-                }),
-            );
-        } catch (error: any) {
-            res.status(500).send(error.message);
-        }
-    }
+			return res.setHeader("Content-Type", "text/html").end(
+				await edge.render("pages/games", {
+					flash_success: `1 Game Found From Search Title: ${String(searchGameTitle).toUpperCase()}`,
+					games: searchedGames,
+					user: global.SESSION_USER,
+					header: Header.games(),
+				}),
+			);
+		} catch (error: any) {
+			res.status(500).send(error.message);
+		}
+	}
 }
