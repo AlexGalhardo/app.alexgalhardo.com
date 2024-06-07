39e3968fb5bfc5b2ac656479c41c948be1e248c0
Alex Galhardo <aleexgvieira@gmail.com>
2024-06-07 09:06:10 -0300

    fix: format

--- src/controllers/web/app.controller.ts
+++ src/controllers/web/app.controller.ts
@@ -4,12 +4,12 @@ import Header from "../../utils/Header";
 import edge from "../../config/edge";
 
 export default class AppController {
-	static async getViewPrivacy(req: Request, res: Response) {
-		return res.setHeader("Content-Type", "text/html").end(
-			await edge.render("pages/privacy", {
-				user: global.SESSION_USER,
-				header: Header.privacy(),
-			}),
-		);
-	}
+    static async getViewPrivacy(req: Request, res: Response) {
+        return res.setHeader("Content-Type", "text/html").end(
+            await edge.render("pages/privacy", {
+                user: global.SESSION_USER,
+                header: Header.privacy(),
+            }),
+        );
+    }
 }
